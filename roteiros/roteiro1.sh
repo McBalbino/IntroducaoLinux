f480c4d89a3fe4ba0531975a0fb87f03131dd942
mcbalbino <cecyliasb@gmail.com>
2022-08-11 15:10:19 -0300

    iniciando roteiro shell

---
+++ roteiros/roteiro1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "======================================================="
+NMAQUINA=$(uname -n)
+echo "Relatório da máquina: " $NMAQUINA
+DATE=$(date '+%a %b %d %T %:::z %Y')
+echo "Data/Hora: " $DATE
+echo "======================================================="
+echo
+
+echo "Maquina ativa desde: " $(uptime -s)
+echo
+
+echo "Versão do kernel: " $(cat /proc/version | awk -F" " '{ print $3 }')
+
+echo
+echo "Memória total: " $(cat /proc/meminfo | grep MemFree | awk -F" " '{ print $2 $3 }')
+
+echo
+
+touch disco.txt
+diskinfo=$(echo $(df -h) > disco.txt)
+echo "Partições: " 
+echo $diskinfo
+
+echo "======================================================="
